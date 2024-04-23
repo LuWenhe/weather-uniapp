b0b154e03d98598d5fbd6d325b8a6010e708d2ac
luckinm <2081647565@qq.com>
2024-04-23 14:29:01 +0800

    增加台风预报功能

--- pages/typhoon/typhoon.vue
+++ pages/typhoon/typhoon.vue
@@ -1,6 +1,6 @@
-<template>
-	<view>
-		
+<!-- <template>
+	<view class="web-view-container">
+		<web-view :src="url" ></web-view>
 	</view>
 </template>
 
@@ -8,15 +8,46 @@
 	export default {
 		data() {
 			return {
-				
+				//url: 'http://typhoon.nmc.cn/web.html',
+				url: 'http://typhoon.nmc.cn/mobile.html',
 			}
-		},
-		created() {
-			console.log('22')
 		},
 		methods: {
 			
-		}
+		},
+			
+			
+
+	}
+</script>
+
+<style>
+	.web-view-container {
+	  position: fixed;
+	  top: -30rpx; /* 设置距离顶部的距离，单位可以根据需要进行调整 */
+	  width: 100%;
+	  height: 1300rpx;
+	}
+</style> -->
+
+
+<template>
+	<view>
+		<web-view :src="url" ></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				//url: 'http://typhoon.nmc.cn/web.html',
+				url: 'https://tf.istrongcloud.com/release/index-fj.html?popularize=false&%23route=&from=timeline&isappinstalled=0',
+			}
+		},
+		methods: {
+			
+		},
 	}
 </script>
 
